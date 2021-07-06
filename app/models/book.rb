49ee3f7850d9d748a226e426ee5e1305312fb794
EC2 Default User <ec2-user@ip-172-31-5-51.ec2.internal>
2021-07-06 07:42:43 +0000

    search ver

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,13 +4,24 @@ class Book < ApplicationRecord
   
   has_many :favorites, dependent: :destroy
 
+  validates :title, presence: true
+  validates :body, presence: true
+  
   def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
   end
   
-
-  validates :title, presence: true
-  validates :body, presence: true
+  def self.search_for(content, method)
+    if method == 'perfect'
+      Book.where(title: content)
+    elsif method == 'forward'
+      Book.where('title LIKE ?', content+'%')
+    elsif method == 'backward'
+      Book.where('title LIKE ?', '%'+content)
+    else
+      Book.where('title LIKE ?', '%'+content+'%')
+    end
+  end
   
   
 
