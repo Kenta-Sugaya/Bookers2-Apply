773fa92e40f35213e975abaa0308ba31b8746f03
EC2 Default User <ec2-user@ip-172-31-5-51.ec2.internal>
2021-07-08 10:58:01 +0000

    like commit

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,6 +3,7 @@ class Book < ApplicationRecord
   has_many :book_comments, dependent: :destroy
   
   has_many :favorites, dependent: :destroy
+  has_many :likes
 
   validates :title, presence: true
   validates :body, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_04_093101) do
+ActiveRecord::Schema.define(version: 2021_07_08_051831) do
 
   create_table "book_comments", force: :cascade do |t|
     t.integer "user_id"
@@ -31,6 +31,13 @@ ActiveRecord::Schema.define(version: 2021_07_04_093101) do
   create_table "favorites", force: :cascade do |t|
     t.integer "user_id"
     t.integer "book_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
